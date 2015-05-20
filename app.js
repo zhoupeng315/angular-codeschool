7b57becac7547a5817edd96781dfd8bad15c88a2
Eric <zhoupeng315@outlook.com>
2015-05-20 18:33:03 -0400

    2st

--- app.js
+++ app.js
@@ -2,5 +2,9 @@
   var gem = { name: 'Azurite', price: 2.95 };
   var app = angular.module('gemStore', []);
   
-  
+  app.controller('StoreController', function(){
+  	this.product = gem;
+
+  });
+
 })();
